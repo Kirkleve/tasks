ea86b061a7f0009fac0b180417bb7565bd56f767
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-05-12 00:59:08 +0200

    №7 remake

--- №7/Program.cs
+++ №7/Program.cs
@@ -7,3 +7,46 @@
 - Numbers – ввывод текущего массива
 - Sum – найдется сумма всех элементов чисел
 */
+
+void Numbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SetNumbers()
+{
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+}
+
+void AddNumbers(int[] numbers)
+{
+
+    int[] sum = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int[] newNumbers = new int[numbers.Length + sum.Length];
+    var result = numbers.Union(sum);
+
+    foreach (int s in result)
+        Console.Write(s);
+}
+
+void RemoveNumbers(int[] array, int remove)
+{
+    int[] newArray = array.Except(new int[] { remove }).ToArray();
+}
+
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int remove = Convert.ToInt32(Console.ReadLine());
+RemoveNumbers(numbers, remove);
+Sum(numbers);
+
+void Sum(int[] array)
+{
+    int rez = array.Sum();
+    Console.WriteLine(rez);
+}
+
+
